d41418849e01deee8f3f9a15032b416588b8e793
marcvegas <marcvegasprv@protonmail.com>
2022-07-17 13:19:57 +0800

    Removed vue-meta import

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Meta from 'vue-meta'
 
 import './index.css'
 import './css/shared.css'
-
-const vueapp = createApp({})
-
-vueapp.use(Meta)
 
 createApp(App).mount('#app')
