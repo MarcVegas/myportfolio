6e58826e660993fe4cd1d3e4bf1f97c2b7209998
marcvegas <marcvegasprv@protonmail.com>
2022-07-17 12:17:14 +0800

    Imported vue-meta

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Meta from 'vue-meta'
 
 import './index.css'
 import './css/shared.css'
+
+const vueapp = createApp({})
+
+vueapp.use(Meta)
 
 createApp(App).mount('#app')
