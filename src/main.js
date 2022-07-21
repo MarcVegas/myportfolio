5ab8d8c73dfce37cb51653315f7e7fde6351ec34
marcvegas <marcvegasprv@protonmail.com>
2022-07-21 20:35:52 +0800

    feat: Imported vueuse/head plugin

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
+import { createHead } from '@vueuse/head'
 import App from './App.vue'
 
 import './index.css'
 import './css/shared.css'
 
 const app = createApp(App)
+const head = createHead()
 
+app.use(head)
 app.mount('#app')
